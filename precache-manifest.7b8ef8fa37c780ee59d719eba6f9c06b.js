44fce95c195bf505bf2bbc5686fe6924f62e2454
EsraaYousef <esraayousef400@gmail.com>
2020-10-29 21:16:15 +0200

    Updates

---
+++ precache-manifest.7b8ef8fa37c780ee59d719eba6f9c06b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f6e1970b6ebdda634ca",
+    "url": "/static/css/main.344d4de7.chunk.css"
+  },
+  {
+    "revision": "5f6e1970b6ebdda634ca",
+    "url": "/static/js/main.cffa4a10.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "05327bd0a5450c2c2a3e",
+    "url": "/static/css/2.6d7289ba.chunk.css"
+  },
+  {
+    "revision": "05327bd0a5450c2c2a3e",
+    "url": "/static/js/2.24424b2b.chunk.js"
+  },
+  {
+    "revision": "72c3c02b3ddbe8e34d27def0661eb0fb",
+    "url": "/static/media/feedback-logo.72c3c02b.png"
+  },
+  {
+    "revision": "c0646797863b3b4baa5c86e5e5fbb0a3",
+    "url": "/static/media/about-bg.c0646797.png"
+  },
+  {
+    "revision": "b7b11b70d3f4e2e602b0ccc244efefc8",
+    "url": "/static/media/bg.b7b11b70.png"
+  },
+  {
+    "revision": "305f7a76ae8b363bf2be8a7e57580c65",
+    "url": "/static/media/beINNormal.305f7a76.svg"
+  },
+  {
+    "revision": "3fb77ab8ad9e2bf0c055c232764d3308",
+    "url": "/static/media/beINNormal.3fb77ab8.ttf"
+  },
+  {
+    "revision": "dedd545d176144bea511b7eda6ad681c",
+    "url": "/static/media/bein-normal.dedd545d.woff2"
+  },
+  {
+    "revision": "8788906d9ada60683ead32e9406b1898",
+    "url": "/static/media/beINNormal.8788906d.woff"
+  },
+  {
+    "revision": "728e0ce5f0f1608c6ed86955ad57a7b4",
+    "url": "/static/media/bein-normal.728e0ce5.eot"
+  },
+  {
+    "revision": "272e2a11b4daa64884a44418b2990a82",
+    "url": "/static/media/beIN-Black.272e2a11.woff2"
+  },
+  {
+    "revision": "2a7b3557b03245b89d8accd9ac7541c8",
+    "url": "/static/media/beINBlack.2a7b3557.woff"
+  },
+  {
+    "revision": "c3b7beb0543d6ce825f0683b46ec51ab",
+    "url": "/static/media/beIN-Black.c3b7beb0.eot"
+  },
+  {
+    "revision": "c9d34c5a6c6dcd1a0daabfc7b2bf88e7",
+    "url": "/static/media/beINBlack.c9d34c5a.ttf"
+  },
+  {
+    "revision": "350d4b0524e607da6f626d0fa730d85c",
+    "url": "/static/media/beINBlack.350d4b05.svg"
+  },
+  {
+    "revision": "b73ed40876fb0f8a9181283c8f3f8a1a",
+    "url": "/index.html"
+  }
+];
